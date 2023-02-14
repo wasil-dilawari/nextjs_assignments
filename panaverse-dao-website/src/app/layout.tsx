2be9c996bbae8094456ad6f61a95c87c1ecd7ed1
wasil-dilawari <wasil.dilawari@gmail.com>
2023-02-14 13:55:36 +0500

    Chakra Integration

--- panaverse-dao-website/src/app/layout.tsx
+++ panaverse-dao-website/src/app/layout.tsx
@@ -1,3 +1,5 @@
+import ChakraWrapper from "src/components/Chakra";
+
 export default function RootLayout({
   children,
 }: {
@@ -6,7 +8,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body>
+        <ChakraWrapper>{children}</ChakraWrapper>
+      </body>
     </html>
   );
 }
